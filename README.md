128ea6525594b77ec43737eb09243ba707604b57
lucaspouchot <lucas.pouchot@digitalairways.com>
2018-04-10 15:22:20 +0200

    update

--- README.md
+++ README.md
@@ -4,9 +4,6 @@ This node allows to send data to [https://thingspeak.com](https://thingspeak.com
 
 It comes form the Thingbox ([http://thethingbox.io](http://thethingbox.io)) but it can be used in any Node-RED installation.
 
-
-
 Just copy the files into a directory called "nodes" within your .node-red directory and restart Node-RED.
 
 Support: thethingbox@gmail.com
-
--- thingspeak.js
+++ thingspeak.js
@@ -1,48 +1,29 @@
-/**
- * Copyright 2014 IBM Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
-var RED = require(process.env.NODE_RED_HOME+"/red/red");
-var request = require('request');
+module.exports = function(RED) {
+  "use strict";
+  var request = require('request');
 
+  function sendThingspeak(api_key, field, value){
+    request.post({
+      url: `http://api.thingspeak.com/update?key=${api_key}&field=${value}`
+    }, function(error, response, body) {
+      if(error) {
+        console.log("[Thingspeak] Error sending data");
+      }
+    });
+  }
 
-function sendThingspeak(api_key, field, value){
-
-	var myUrl = "http://api.thingspeak.com/update?key="+api_key+"&"+field+"="+value;
-
-	console.log("\n Thingspeak URL :\n"+myUrl);
-	
-	request.post({
-		url : myUrl
-	}, function(error, response, body) {
-		if(error)
-			console.log("\n[Thingspeak] Error sending data\n");
-	});
-}
-
-function ThingspeakNode(n) {
-
-	RED.nodes.createNode(this,n);
+  function ThingspeakNode(n) {
+    RED.nodes.createNode(this,n);
+    this.api_key = n.api_key;
+    this.field = n.field;
+    var node = this;
 
     this.on("input", function(msg) {
-    	
-		var api_key = msg.api_key||n.api_key;
-		var field = msg.field||n.field;
-		var value = msg.payload;
-
-		sendThingspeak(api_key, field, value);
+      var api_key = msg.api_key||node.api_key;
+      var field = msg.field||node.field;
+      var value = msg.payload;
+      sendThingspeak(api_key, field, value);
     });
-}
-
-RED.nodes.registerType("thingspeak",ThingspeakNode);
+  }
+  RED.nodes.registerType("thingspeak", ThingspeakNode);
+};
